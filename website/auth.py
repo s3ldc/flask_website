caefa3f74b282b637ccdc5af685caa2342f2782e
Biriy <biriyasunil25@gmail.com>
2025-12-12 12:06:33 +0530

    Add signup form validation

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
@@ -12,4 +12,21 @@ def logout():
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.method.get('email')
+        firstName = request.method.get('firstName')
+        password1 = request.method.get('password1')
+        password2 = request.method.get('password2')
+
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+            # add user to database
+            pass
     return render_template("sign_up.html")
