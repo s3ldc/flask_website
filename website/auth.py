7dbdf3ff4bd0e6bf4c04a09a11263c3ff6b5e942
Biriy <biriyasunil25@gmail.com>
2025-12-18 16:25:20 +0530

    Add user authentication with Flask-Login

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -15,6 +16,8 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in Successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, Try again!', category='error')
         else:
@@ -22,8 +25,10 @@ def login():
     return render_template("login.html", boolean = True)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return '<p>Logout</p>'
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -48,6 +53,7 @@ def signup():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash("Account Created!", category="success")
             return redirect(url_for('views.home'))
 
